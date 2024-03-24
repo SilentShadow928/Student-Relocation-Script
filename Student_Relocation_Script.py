44e85150cb9845f6f8a291b66d11e69e1813e2f8
SilentShadow928 <78055461+SilentShadow928@users.noreply.github.com>
2024-03-24 16:46:59 -0700

    Fix spelling error

--- Student_Relocation_Script.py
+++ Student_Relocation_Script.py
@@ -3,7 +3,7 @@ This is the Student Relocation Script.
 Terminology:
 DS - dissaproval score. This is how much the student and other students dislike the student being located there. The LOWER the better.
 Kick - move a student from a group into a group with only them in it
-This script takes data from a excel spreadsheet that is experted from a microsoft form.
+This script takes data from a excel spreadsheet that is exported from a microsoft form.
 The form's fist question should be required, the other should not be.
 The form's format should be this:
 What is your name? Answer in this format: First name [space] last name (Example: John Doe)
