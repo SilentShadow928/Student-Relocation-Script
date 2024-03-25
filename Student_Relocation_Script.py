a80217e82b8fa25a90c3d8eb993661cb5a08e158
SilentShadow928 <78055461+SilentShadow928@users.noreply.github.com>
2024-03-24 17:01:08 -0700

    Update Student_Relocation_Script.py

--- Student_Relocation_Script.py
+++ Student_Relocation_Script.py
@@ -1,18 +1,3 @@
-"""
-This is the Student Relocation Script.
-Terminology:
-DS - dissaproval score. This is how much the student and other students dislike the student being located there. The LOWER the better.
-Kick - move a student from a group into a group with only them in it
-This script takes data from a excel spreadsheet that is exported from a microsoft form.
-The form's fist question should be required, the other should not be.
-The form's format should be this:
-What is your name? Answer in this format: First name [space] last name (Example: John Doe)
-Who do you want to sit with? Answer in the same format as the previous question.
-Who else do you want to sit with? Answer in the same format as the previous question.
-Who else do you want to sit with? Answer in the same format as the previous question.
-Who else do you want to sit with? Answer in the same format as the previous question.
-"""
-#If you end up improving this script, please send a pull request to [Insert repo here]
 print("Please wait...")
 import msvcrt # For instant input
 import random
